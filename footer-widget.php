c859df20d448dea66965df2c370510b046c5dfc5
Cath Riddoch <cathriddoch@gmail.com>
2021-03-04 16:07:58 +0000

    footer done

---
+++ footer-widget.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) ) {?>
+        <div id="footer-widget" class="row m-0 <?php if(!is_theme_preset_active())?>">
+            <div class="container">
+                <div class="row">
+                    <?php if ( is_active_sidebar( 'footer-1' )) : ?>
+                        <div class="col-12 col-md-6"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer-2' )) : ?>
+                        <div class="col-12 col-md-6 text-center"><?php dynamic_sidebar( 'footer-2' ); ?></div>
+                    <?php endif; ?>
+                    
+                </div>
+            </div>
+        </div>
+
+<?php }
