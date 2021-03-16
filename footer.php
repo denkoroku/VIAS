1c3f164c87d1c78932f987a82431513129016ef4
Cath Riddoch <cathriddoch@gmail.com>
2021-03-16 15:13:01 +0000

    removes colophon from footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+<?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</div><!-- #content -->
+    <?php get_template_part( 'footer-widget' ); ?>
+	<footer>
+	</footer><!-- #colophon -->
+<?php endif; ?>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
